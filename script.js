a6077214ed92352250dce037a0c42a41bcb92fff
HichemKhalf <h@mayssdigital.com>
2022-12-13 13:27:18 +0100

    overlay menu first attempt

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const menuBtn = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.links_mobile');
+const heronav = document.querySelector('.heronav');
 let menuOpen = false;
 
 menuBtn.addEventListener('click' , () => {
     if(!menuOpen){
         menuBtn.classList.add('open');
+        mobileMenu.classList.add('open');
+        heronav.classList.add('overlay');
         menuOpen = true;
     }
     else {
         menuBtn.classList.remove('open');
+        mobileMenu.classList.remove('open');
+        heronav.classList.remove('overlay');
         menuOpen = false;
     }
 });
