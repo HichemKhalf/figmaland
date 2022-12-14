777043722993a0e5228ab46a1c6f2f57f7d52576
HichemKhalf <h@mayssdigital.com>
2022-12-14 09:36:50 +0100

    working mobile menu

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 const menuBtn = document.querySelector('.hamburger');
+const menuWrap = document.querySelector('.menu_wrap');
 const mobileMenu = document.querySelector('.links_mobile');
-const heronav = document.querySelector('.heronav');
+const overlay = document.querySelector('.overlay');
+const menuItem = document.querySelectorAll('.menu_item');
 let menuOpen = false;
 
 menuBtn.addEventListener('click' , () => {
     if(!menuOpen){
         menuBtn.classList.add('open');
         mobileMenu.classList.add('open');
-        heronav.classList.add('overlay');
+        menuWrap.classList.add('open');
+        overlay.classList.add('open');
         menuOpen = true;
     }
     else {
         menuBtn.classList.remove('open');
         mobileMenu.classList.remove('open');
-        heronav.classList.remove('overlay');
+        overlay.classList.remove('open');
+        menuWrap.classList.remove('open');
         menuOpen = false;
     }
 });
 
+for (var i = 0; i < menuItem.length; i++) {
 
+    menuItem[i].addEventListener('click' , () => {
+    
+            menuBtn.classList.remove('open');
+            mobileMenu.classList.remove('open');
+            overlay.classList.remove('open');
+            menuWrap.classList.remove('open');
+            
+        
+    });
+
+}
